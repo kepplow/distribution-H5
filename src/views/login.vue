9af17e6f04244cbdcdb5a5c75f8be7aed6c8bfe6
lilei <1184841584@qq.com>
2020-06-17 18:04:14 +0800

    验证码

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,10 +3,10 @@
     <van-form @submit="onSubmit">
       <van-field
         v-model="phone"
-        name="用户名"
-        label="用户名"
-        placeholder="用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        name="手机号"
+        label="手机号"
+        placeholder="手机号"
+        :rules="[{ required: true, message: '请填写手机号' }]"
       />
       <van-field
         v-model="password"
@@ -14,8 +14,27 @@
         name="密码"
         label="密码"
         placeholder="密码"
+        class="mt-1"
         :rules="[{ required: true, message: '请填写密码' }]"
       />
+
+      <div class="flex">
+        <van-field
+          v-model="Vcode"
+          type="text"
+          name="验证码"
+          label="验证码"
+          placeholder="验证码"
+          class="mt-1"
+          :rules="[{ required: true, message: '请填写验证码' }]"
+        />
+        <div id="picyzm" class="mt-1 ml-1"></div>
+      </div>
+
+      <div class="flex justify-end">
+        <button class="btn btn-link">注册</button>
+        <button class="btn btn-link">忘记密码</button>
+      </div>
       <div style="margin: 16px;">
         <van-button round block type="info" native-type="submit">登录</van-button>
       </div>
@@ -27,38 +46,58 @@
 </template>
 
 <script>
+import "../../public/gVerify/gVerify";
 export default {
   data() {
     return {
       show: false,
       phone: "18112312313",
-      password: "123456"
+      password: "123456",
+      Vcode: "",
+      TCode: "abcd",
+      verifyCode: null
     };
   },
   methods: {
     onSubmit() {
-      this.WS.sendMsg({
-        code: 1200,
-        args: {
-          phone: this.phone,
-          password: this.password
-        }
-      }).then(res => {
-        console.log("登录信息:", res);
-        if (res.args.uid) {
-          this.show = true;
-          localStorage.setItem("Uid", JSON.stringify(res.args.uid));
-          localStorage.setItem("loginInfo", JSON.stringify(res.args));
-          localStorage.setItem("userPhone", this.phone);
-          localStorage.setItem("userPWD", this.password);
-          setTimeout(() => {
-            this.$router.push("/home");
-          }, 500);
-        }
-      });
+      let res = this.verifyCode.validate(this.Vcode);
+
+      if (res) {
+        console.log(1111, this.Vcode);
+      } else {
+        console.log(2222, this.Vcode);
+      }
+      // this.WS.sendMsg({
+      //   code: 1200,
+      //   args: {
+      //     phone: this.phone,
+      //     password: this.password
+      //   }
+      // }).then(res => {
+      //   console.log("登录信息:", res);
+      //   if (res.args.uid) {
+      //     this.show = true;
+      //     localStorage.setItem("Uid", JSON.stringify(res.args.uid));
+      //     localStorage.setItem("loginInfo", JSON.stringify(res.args));
+      //     localStorage.setItem("userPhone", this.phone);
+      //     localStorage.setItem("userPWD", this.password);
+      //     setTimeout(() => {
+      //       this.$router.push("/home");
+      //     }, 500);
+      //   }
+      // });
     }
   },
-  beforeMount() {}
+  mounted() {
+    let verifyCode;
+    this.$nextTick(() => {
+      this.verifyCode = new GVerify({
+        id: "picyzm",
+        type: "blend",
+        code: this.TCode
+      });
+    });
+  }
 };
 </script>
 
